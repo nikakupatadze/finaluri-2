af8acc8a0cc838903c2cdecbb0b3ec0a9e975f18
nikakupatadze <123635452+nikakupatadze@users.noreply.github.com>
2023-05-02 01:50:39 +0400

    add fech and light modde

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ const logo = document.querySelector('#logo');
 const logo1 = document.querySelector('#logo1');
 const body = document.querySelector('body');
 const DivSearch = document.querySelector('#DivSearch');
+const dark = document.querySelector('#dark');
 
-console.log(container);
+
 lightMode.addEventListener('click', () => {
-    // DivSearch.classList.toggle('show');
     body.classList.toggle('show');
     lightMode.classList.toggle('show1')
     container.classList.toggle('show')
     DivSpace.classList.toggle('show')
     DivlightMode.classList.toggle('show')
     logo.classList.toggle('show1')
-    logo1.classList.toggle('show1')
+    logo1.classList.toggle('show2')
+    dark.classList.toggle('dark')
+});
+
+(function () {
+const getUsers = async () => {
+    const result = await fetch('https://api.github.com/users');
+    const  users = result.json();
+    console.log(users);
+    
+};
+getUsers()
 });

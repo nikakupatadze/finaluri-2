5dfccda4676dcc08e6991a7c35ad97a95684c2b6
nikakupatadze <123635452+nikakupatadze@users.noreply.github.com>
2023-05-09 17:31:03 +0400

    add github users

--- index.js
+++ index.js
@@ -1,101 +1,3 @@
-
-
-// (function () {
-// const getUsers = async () => {
-//     const result = await fetch('https://api.github.com/users');
-//     const  users = result.json();
-//     console.log(users);
-    
-// };
-// getUsers()
-// });
-
-
-
-// const APIURL = 'https://api.github.com';
-
-// const form = document.querySelector('#form');
-// const main = document.querySelector('#main');
-// const search = document.querySelector('#search');
-// const button = document.querySelector('#button');
-
-// async function getUsers(username) {
-//     try{
-//         const { data } = await axios(APIURL + username);
-//         createUsersCard(data);
-//         getRepos(username);
-//     }catch (error) {
-//         if(error.response.status == 404){
-//             createErrorCard('No profile found wiht this username'); 
-//         }
-//     }
-// }
-
-// async function getRepos(username){
-//     try{
-//         const { data } = await axios(APIURL + username + '/repos?sort=created');
-//         addReposToCard(data);
-//     }catch{
-//         createErrorCard('problem fetching Repos');
-//     }
-// }
-
-// function createUsersCard(user){
-//     const cardHTML = `
-//     <div class="card">
-//     <div>
-//         <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
-//     </div>
-//     <div class="user-info">
-//         <h2>${user.name}</h2>
-//         <p>${user.bio}</p>
-//         <ul>
-//             <li>${user.followers} <strong>Followers</strong></li>
-//             <li>${user.following} <strong>Following</strong></li>
-//             <li>${user.public_repos} <strong>Repos</strong></li>
-//         </ul>
-//         <div id="Repos"></div>
-//     </div>
-// </div>
-//     `
-//     main.innerHTML = cardHTML;
-// }
-
-// function createErrorCard(msg){
-//    const cardHTML = `  <div class="card">
-//     <h1>${msg}</h1>
-// </div>`
-// main.innerHTML = cardHTML;
-// }
-
-// function addReposToCard(repos){
-//     const reposEl = document.getElementById('repos');
-
-//     repos
-//        .slice(0, 5)
-//        .forEach(repo => {
-//           const repoEl = document.createElement ('a');
-//           repoEl.classList.add('repo');
-//           repoEl.hreaf = repo.html_url;
-//           repoEl.target = '_black';
-//           repoEl.innerText = repo.name
-
-//           reposEl.appendChild(repoEl);
-//        });
-// }
-
-// form.addEventListener('click', (e) => {
-//     e.preventDefault();
-
-//     const user = search.value;
-
-//     if (user) {
-//         getUsers(user);
-
-//         search.value = '';
-//     }
-// });
-
  const APIURL = 'https://api.github.com/users/'
 
 const main = document.getElementById('main')
@@ -138,7 +40,6 @@ function createUserCard(user){
                 <h2 class="user_name" >${user.name}</h2>
                 <h3 class="user_account">@${user.login}</h3> 
                 <p class="user_bio">${user.bio}</p>
-                <h4> ${user.date}</h4>
                 </div>
                 
                 <ul>
@@ -228,6 +129,4 @@ lightMode.addEventListener('click', () => {
     logo.classList.toggle('show1')
     logo1.classList.toggle('show2')
     dark.classList.toggle('dark1')
-    // user.cardHTML.classList.toggle('show')
-    // console.log(user);
 });
